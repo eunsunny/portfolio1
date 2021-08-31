b1e7782e29d66e23b51ec5e36d5a81b62d3829f2
ghnm89 <ghnm89@naver.com>
2021-08-31 17:45:30 +0900

    2021-08-31
    ReportVO 변경(변경된 컬럼에 맞게 변수 추가)

--- src/main/java/com/green/sunny/dto/ReportVO.java
+++ src/main/java/com/green/sunny/dto/ReportVO.java
@@ -1,11 +1,22 @@
 package com.green.sunny.dto;
 
+import java.util.Date;
+
 public class ReportVO {
+	private int rseq;
 	private String id;
+	private String report_id;
 	private String rp_kind;
 	private String content;
 	private String result;
+	private Date report_date;
 	
+	public Date getReport_date() {
+		return report_date;
+	}
+	public void setReport_date(Date report_date) {
+		this.report_date = report_date;
+	}
 	public String getId() {
 		return id;
 	}
@@ -30,10 +41,22 @@ public class ReportVO {
 	public void setResult(String result) {
 		this.result = result;
 	}
+	public int getRseq() {
+		return rseq;
+	}
+	public void setRseq(int rseq) {
+		this.rseq = rseq;
+	}
+	public String getReport_id() {
+		return report_id;
+	}
+	public void setReport_id(String report_id) {
+		this.report_id = report_id;
+	}
 	
 	@Override
 	public String toString() {
-		return "ReportVO [id=" + id + ", rp_kind=" + rp_kind + ", content=" + content + ", result=" + result + "]";
+		return "ReportVO [rseq=" + rseq + ", id=" + id + ", report_id=" + report_id + ", rp_kind=" + rp_kind
+				+ ", content=" + content + ", result=" + result + "]";
 	}
-
 }
